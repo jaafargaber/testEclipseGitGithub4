2a9ccc3fdc9cda8ab8e2538cefaa8d8e4097f44b
gaber <jaafar.gaber@gmail.com>
2021-01-05 15:24:06 +0100

    commit 4

--- src/main/java/EclipseGitGithub2/EclipseGitGithub2/App.java
+++ src/main/java/EclipseGitGithub2/EclipseGitGithub2/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Bonjour EPSI 3.....!" );
+        System.out.println( "Bonjour EPSI 4.....!" );
     }
 }
